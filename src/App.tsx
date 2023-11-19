987030899bd9b52da28eacc13c51b679bb45fee6
akashmadduru <akashmadduru@gmail.com>
2023-11-19 10:38:05 +0530

    usestate, use effect demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Home from "./pages/Home";
+import { Box, ChakraProvider, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-// changing commit name
+interface DataInterface {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const [data, setData] = useState<DataInterface[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
   return (
     <ChakraProvider>
-      <Home />
+      <Box>
+        {data.map((d, i) => (
+          <Box key={i}>
+            <Text>{d.id}</Text>
+            <Text>{d.title}</Text>
+          </Box>
+        ))}
+      </Box>
     </ChakraProvider>
   );
 }
